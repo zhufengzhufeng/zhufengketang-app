dfc7412db67f5dc111455961deafe088054ca221
zhufengzhufeng <894918097@qq.com>
2017-08-23 19:48:12 +0800

    loadMore

--- src/components/ScrollList/index.js
+++ src/components/ScrollList/index.js
@@ -5,14 +5,26 @@ import React,{Component} from 'react';
 //hasMore 是否有更多
 //loadMore获取更多的方法
 export default class ScrollList extends Component{
+    constructor(){
+        super();
+        this.state = {flag:false}
+    }
     //等待接收的属性有element 在绑定事件
     componentWillReceiveProps(nextProps){ //父组件数据更新 会触发子组件的WillReceiveProps
-        if(nextProps.element){
+        if(nextProps.element && !this.state.flag){
+            //节流
             nextProps.element.addEventListener('scroll',()=>{
                 //....... 判断 scrollTop,offsetHeight,scrollHeight
-                let {scrollTop,offsetHeight,scrollHeight} = nextProps.element;
-                console.log(scrollTop,offsetHeight,scrollHeight);
-            })
+                clearTimeout(this.timer);
+                this.timer = setTimeout(()=>{
+                    let {scrollTop,offsetHeight,scrollHeight} = nextProps.element;
+                    //距离底部还有20像素就开始加载
+                    if(scrollTop+offsetHeight+20 >scrollHeight&&this.props.hasMore&&!this.props.isLoading){
+                        this.props.loadMore();
+                    }
+                },50);
+            });
+            this.setState({flag:true});
         }
     }
     render(){

c3e2154a4a549b40057fcb64efd3a654626c5928
zhufengzhufeng <894918097@qq.com>
2017-08-23 09:58:16 +0800

    环境搭建

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry:'./src/main.js',
+    output: {
+      filename:'bundle.js',
+      path:path.resolve('./dist')
+    },
+    module: {
+        rules:[
+            {test:/\.js$/,use:'babel-loader',exclude:/node_modules/},
+            {test:/\.css$/,use:['style-loader','css-loader']},
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+            {test:/\.(jpg|png|gif)$/,use:'url-loader'}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./index.html'
+        })
+    ]
+};
